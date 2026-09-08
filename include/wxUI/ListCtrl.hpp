a840cd52f3cb467283d7b789f44337b34cb90220
owacoder <owacoder@gmail.com>
2026-09-08 10:48:06 -0400

    Implemented options for ListCtrl

--- include/wxUI/ListCtrl.hpp
+++ include/wxUI/ListCtrl.hpp
@@ -31,6 +31,66 @@ SOFTWARE.
 
 namespace wxUI {
 
+namespace details {
+
+inline auto toWxListItem(auto &&text) {
+    wxListItem item;
+    item.SetText(std::forward<decltype(text)>(text));
+    return item;
+}
+
+template <details::utf8_text_choice T>
+inline auto convertToListItems(std::initializer_list<T> choices) -> std::vector<wxListItem>
+{
+    std::vector<wxListItem> result;
+    result.reserve(choices.size());
+    for (auto const& choice : choices) {
+        result.push_back(details::toWxListItem(choice));
+    }
+    return result;
+}
+
+inline auto flattenToListItems(std::initializer_list<std::initializer_list<char const*>> choices) -> std::vector<wxListItem>
+{
+    auto count = static_cast<size_t>(0);
+    for (auto const& group : choices) {
+        count += group.size();
+    }
+    auto result = std::vector<wxListItem> {};
+    result.reserve(count);
+    for (auto const& group : choices) {
+        for (auto const* choice : group) {
+            result.push_back(details::toWxListItem(choice));
+        }
+    }
+    return result;
+}
+
+// Convert variadic arguments to vector of wxString
+template <details::utf8_text_choice... Strings>
+inline auto toVectorListItems(Strings&&... strings) -> std::vector<wxListItem>
+{
+    auto result = std::vector<wxListItem> {};
+    result.reserve(sizeof...(strings));
+    (result.push_back(details::toWxListItem(std::forward<Strings>(strings))), ...);
+    return result;
+}
+
+template <utf8_text_input_range Range>
+inline auto ToVectorListItems(Range&& range) -> std::vector<wxListItem>
+{
+    auto result = std::vector<wxListItem> {};
+    if constexpr (std::ranges::sized_range<Range>) {
+        result.reserve(std::ranges::size(range));
+    }
+    std::ranges::transform(std::forward<Range>(range), std::back_inserter(result), [](auto const& choice) {
+        return details::toWxListItem(choice);
+    });
+    return result;
+}
+
+}
+
 // https://docs.wxwidgets.org/latest/classwx_list_ctrl.html
 struct ListCtrl {
     using underlying_t = wxListCtrl;
@@ -70,19 +130,19 @@ struct ListCtrl {
     requires details::utf8_text_choice<String>
     ListCtrl(wxWindowID identity, std::initializer_list<String> choices)
         : details_(identity)
-        , choices_(details::Ranges::convertTo(choices))
+        , items_(details::Ranges::convertToListItems(choices))
     {
     }
 
     ListCtrl(wxWindowID identity, std::initializer_list<std::initializer_list<char const*>> choices)
         : details_(identity)
-        , choices_(details::Ranges::flattenToUtf8(choices))
+        , items_(details::Ranges::flattenToListItems(choices))
     {
     }
 
     ListCtrl(wxWindowID identity, details::Ranges::utf8_text_input_range auto&& choices)
         : details_(identity)
-        , choices_(details::Ranges::ToVectorUtf8(std::forward<decltype(choices)>(choices)))
+        , items_(details::Ranges::ToVectorListItems(std::forward<decltype(choices)>(choices)))
     {
     }
 
@@ -90,8 +150,56 @@ struct ListCtrl {
     requires(sizeof...(Strings) > 0) && (details::utf8_text_choice<Strings> && ...)
     ListCtrl(wxWindowID identity, Strings&&... choices)
         : details_(identity)
-        , choices_(details::Ranges::toVectorUtf8(std::forward<Strings>(choices)...))
+        , items_(details::Ranges::toVectorListItems(std::forward<Strings>(choices)...))
     {
+    }
+
+    auto withItems(std::vector<wxListItem> items) & -> ListCtrl&
+    {
+        items_ = std::move(items);
+        return *this;
+    }
+
+    auto withItems(std::vector<wxListItem> items) && -> ListCtrl&&
+    {
+        items_ = std::move(items);
+        return *this;
+    }
+
+    auto withColumns(std::vector<wxListItem> columns) & -> ListCtrl&
+    {
+        columns_ = std::move(columns);
+        return *this;
+    }
+
+    auto withColumns(std::vector<wxListItem> columns) && -> ListCtrl&&
+    {
+        columns_ = std::move(columns);
+        return *this;
+    }
+
+    auto withNormalImages(wxVector<wxListItem> images) & -> ListCtrl&
+    {
+        normalImages_ = std::move(images);
+        return *this;
+    }
+
+    auto withNormalImages(wxVector<wxListItem> images) && -> ListCtrl&&
+    {
+        normalImages_ = std::move(images);
+        return *this;
+    }
+
+    auto withSmallImages(wxVector<wxListItem> images) & -> ListCtrl&
+    {
+        smallImages_ = std::move(images);
+        return *this;
+    }
+
+    auto withSmallImages(wxVector<wxListItem> images) && -> ListCtrl&&
+    {
+        smallImages_ = std::move(images);
+        return *this;
     }
 
     auto setSelection(int which) & -> ListCtrl&
@@ -207,21 +315,48 @@ struct ListCtrl {
 
 private:
     details::WidgetDetails<ListCtrl, underlying_t> details_;
-    std::vector<wxString> choices_ {};
+    std::vector<wxListItem> columns_ {};
+    std::vector<wxListItem> items_ {};
     std::vector<int> selection_;
     std::optional<int> ensureVisible_ {};
+    std::optional<wxVector<wxBitmapBundle>> normalImages_ {};
+    std::optional<wxVector<wxBitmapBundle>> smallImages_ {};
 
     template <typename Parent>
     auto createImpl()
     {
-        return [&choices = choices_, &selections = selection_, &ensureVisible = ensureVisible_](Parent* parent, wxWindowID id, wxPoint pos, wxSize size, int64_t style) {
-            auto* widget = customizations::ParentCreate<underlying_t>(parent, id, pos, size, static_cast<int>(choices.size()), choices.data(), style);
+        return [&columns = columns_, 
+            &items = items_,
+            &normalImages = normalImages_,
+            &smallImages = smallImages_,
+            &selections = selection_,
+            &ensureVisible = ensureVisible_](Parent* parent, wxWindowID id, wxPoint pos, wxSize size, int64_t style) {
+            auto* widget = customizations::ParentCreate<underlying_t>(parent, id, pos, size, style);
+
+            if (normalImages) {
+                widget->SetNormalImages(*normalImages);
+            }
+
+            if (smallImages) {
+                widget->SetSmallImages(*smallImages);
+            }
+
+            for (long index {}; auto &&column : columns) {
+                widget->InsertColumn(index++, column);
+            }
+
+            for (auto &&item : items) {
+                widget->InsertItem(item);
+            }
+
             for (auto&& selection : selections) {
                 widget->SetSelection(selection);
             }
+
             if (ensureVisible) {
                 widget->EnsureVisible(*ensureVisible);
             }
+
             return widget;
         };
     }
